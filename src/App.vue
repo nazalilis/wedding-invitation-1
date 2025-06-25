3b7eba854888695af4ec566790f4ff7acdfd27f7
nazaliss <kholisnazali27@gmail.com>
2025-06-26 02:34:40 +0700

    Membuat Template undangan

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+	import hero from "./components/hero.vue";
+	import home from "./components/home.vue";
+</script>
+
+<template>
+	<hero />
+	<home />
+</template>
+
+<style scoped></style>
---
+++ src/components/hero.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+	<div class="hero min-h-screen" style="background-image: url(/src/assets/img/bg.webp)">
+		<div class="hero-overlay"></div>
+		<div class="hero-content text-neutral-content text-center">
+			<div class="max-w-md">
+				<h1 class="mb-5 text-5xl font-bold font-sacramento">Hello there</h1>
+				<p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+				<button class="btn btn-outline btn-secondary">Lihat Undangan</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped></style>
